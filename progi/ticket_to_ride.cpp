095b17f04d5c20ed566c2d0f06faf3c7bd9a1639
CalnguinhoS2S2 <126282504+CalnguinhoS2S2@users.noreply.github.com>
2023-12-07 18:15:11 -0300

    Update ticket_to_ride.cpp

--- progi/ticket_to_ride.cpp
+++ progi/ticket_to_ride.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 const int maxn = 40;
 
-map<string, map<string, int>> cidade;
+map<string, map<string, pair<int, string>>> cidades;
 bool vis[maxn]={0};
 int trens[maxn]={0};
 set<string> ord;
 vector<string> city={};
 
-void iago{
-	cidades["vancouver"]["seattle"] = 1;
-	cidades["seattle"]["vancouver"] = 1;
-	cidades["vancouver"]["calgary"] = 3;
-	cidades["calgary"]["vancouver"] = 3;
-	cidades["calgary"]["seattle"] = 4;
-	cidades["seattle"]["calgary"] = 4;
-	cidades["calgary"]["winnipeg"] = 6;
-	cidades["calgary"]["helena"] = 4;
+void coneccoes(){
+	cidades["vancouver"]["seattle"] = (1,"cinza");
+	cidades["seattle"]["vancouver"] = (1,"cinza");
+	cidades["vancouver"]["calgary"] = (3,"cinza");
+	cidades["calgary"]["vancouver"] = (3,"cinza");
+	cidades["calgary"]["seattle"] = (4,"cinza");
+	cidades["seattle"]["calgary"] = (4,"cinza");
+	cidades["calgary"]["winnipeg"] = (6,"branco");
+	cidades["calgary"]["helena"] = (4,"cinza");
 	cidades["portland"]["salt lake city"] = 6;
 	cidades["portland"]["san francisco"] = 5;
 	cidades["helena"]["seattle"] = 6;
@@ -44,19 +44,4 @@ void dfs(string i){
 }
 
 int main(){
-	
-
-
-    int cont=0;
-    for(int i=0; i<37; i++){
-        string Cids;cin>>Cids;
-        ord.insert(Cids);
-        if(!ord.count(Cids)) cont++;
-        int n;cin>>n; // numero de cidades que uma cidade conecta
-        for(int j=0; j<n; j++){
-            string a;
-            cin>>a;
-            cidades[{Cids, cont}].push_back(a);
-        }
-    }
 }
