234b1deb2889bd88792c033b95e0da03cef00a18
CalnguinhoS2S2 <126282504+CalnguinhoS2S2@users.noreply.github.com>
2023-12-07 10:13:50 -0300

    Update ticket_to_ride.cpp

--- progi/ticket_to_ride.cpp
+++ progi/ticket_to_ride.cpp
@@ -4,53 +4,49 @@ using namespace std;
 
 const int maxn = 40;
 
-vector<pair<string, int>> cidades[maxn]; // 36 cidades
+map<string, map<string, int>> cidade;
 bool vis[maxn]={0};
 int trens[maxn]={0};
 set<string> ord;
-vector<string> city={"Caracas", 
-	"Bocota", 
-	"Boa Vista", 
-	"Pico Da Neblina", 
-	"Cruzeiro Do Sul", 
-	"Lima", 
-	"Tabatinga", 
-	"Manaus", 
-	"Porto Velho", 
-	"Rio Branco",
-	"La Paz",
-	"Belem",
-	"Altamira",
-	"SA. Dos Carajas",
-	"Cuiaba",
-	"Campo Grande",
-	"Assuncao",
-	"Buenos Aires",
-	"Sao Luis",
-	"Fortaleza",
-	"Teresina",
-	"Juazeiro",
-	"Palmas",
-	"Recife",
-	"Salvador",
-	"Brasilia",
-	"Uberlandia",
-	"BH",
-	"Porto Seguro",
-	"Vitoria",
-	"Rio de Janeiro",
-	"Sao Paulo",
-	"Foz Do Iguacu",
-	"Curitiba",
-	"Porto Alecre",
-	"Montevideu"};
+vector<string> city={};
 
+void iago{
+	cidades["vancouver"]["seattle"] = 1;
+	cidades["seattle"]["vancouver"] = 1;
+	cidades["vancouver"]["calgary"] = 3;
+	cidades["calgary"]["vancouver"] = 3;
+	cidades["calgary"]["seattle"] = 4;
+	cidades["seattle"]["calgary"] = 4;
+	cidades["calgary"]["winnipeg"] = 6;
+	cidades["calgary"]["helena"] = 4;
+	cidades["portland"]["salt lake city"] = 6;
+	cidades["portland"]["san francisco"] = 5;
+	cidades["helena"]["seattle"] = 6;
+	cidades["helena"]["winnipeg"] = 4;
+	cidades["helena"]["calgary"] = 4;
+	cidades["helena"]["salt lake city"] = 3;
+	cidades["helena"]["denver"] = 4;
+	cidades["helena"]["omaha"] = 5;
+	cidades["helena"]["duluth"] = 6;
+	cidades["winnipeg"]["calgary"] = 6;
+	cidades["winnipeg"]["helena"] = 4;
+	cidades["winnipeg"]["duluth"] = 4;
+	cidades["winnipeg"]["sault st. marie"] = 6;
+	cidades["portland"]["seattle"] = 1;
+	cidades["san francisco"]["portland"] = 5;
+	cidades["san francisco"]["salt lake city"] = 5;
+	cidades["san francisco"]["los angeles"] = 3;
+
+}
 
 void dfs(string i){
 	//vis[cidades[i]]=1;
 }
 
 int main(){
+	
+
+
     int cont=0;
     for(int i=0; i<37; i++){
         string Cids;cin>>Cids;
