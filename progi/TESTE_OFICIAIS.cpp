1a06de976a17277bc37220e1c9e5674ef27a1d47
CalanguinhoS2S2 <126282504+CalnguinhoS2S2@users.noreply.github.com>
2024-01-22 16:55:46 -0300

    Update TESTE_OFICIAIS.cpp

--- progi/TESTE_OFICIAIS.cpp
+++ progi/TESTE_OFICIAIS.cpp
@@ -1,14 +1,56 @@
 #include <bits/stdc++.h>
 #define sz(x) ((int)x.size())
+#define ff first
+#define ss second
 using namespace std;
 
-int main(){
-	set<string> st={"abc", "bac", "cba", "acb"};
+const int maxn=1e5;
 
+vector<int> adj[maxn];
+vector<string> nodes;
+bool vis[maxn]={0};
+
+void dfs(int v){
+	vis[v]=1;
+	for(auto x:adj[v]){
+		if(!vis[x]) dfs(x);
+	}
+}
+
+int main(){
 	int n; cin>>n;
+	cout<<'\n';
 	while(n--){
-		string a; cin>>a;
-		if(st.count(a)) cout<<"YES\n";
-		else cout<<"NO\n";
+		int cont=0;
+		char nome;cin>>nome;
+		int tam=nome-64;
+		vector<int> aux;
+		for(int i=0; i<tam; i++){
+			aux.push_back(i);
+		}
+		cin.ignore();
+		string nod;
+		getline(cin,nod);
+		while(sz(nod)){
+			stringstream ss(nod);
+			string token;
+			while(ss>>token){
+				nodes.push_back(token);
+				adj[token[1]-65].push_back(token[2]-65);
+			}
+			getline(cin, nod);
+		}
+		for(auto x:aux){
+			if(!vis[x]){
+				dfs(x);
+				cont++;
+			}
+		}
+
+		for(auto x:aux)cout<<x<<' ';
+		cout<<'\n';
+		for(int i=0; i<tam; i++)cout<<vis[i]<<' ';
+		cout<<'\n';
+		cout<<cont<<"\n\n";
 	}
 }
