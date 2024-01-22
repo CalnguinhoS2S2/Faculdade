1c3cd697788f8697127bd5292c62f686c623d0f2
CalanguinhoS2S2 <126282504+CalnguinhoS2S2@users.noreply.github.com>
2024-01-22 11:10:32 -0300

    Update 1.cpp

--- progi/1.cpp
+++ progi/1.cpp
@@ -5,54 +5,49 @@
 using namespace std;
 
 const int maxn=21;
-
+int cnt=0;
 char mt[maxn][maxn];
 bool vis[maxn][maxn]={0};
 int r, c, dist[maxn][maxn]={0}, vi[4] = {-1, 0, 1, 0}, vj[4] = { 0, 1, 0,-1};
+vector<pair<int, int>> coor;
 
-void show(){for(int z=0;z<r;z++){for(int y=0;y<c;y++){cout << mt[z][y] << " \n"[y+1==c];}}cout << "\n";}
+void show(){for(int z=0;z<r;z++){for(int y=0;y<c;y++){cout << mt[z][y];}cout<<'\n';}cout << '\n';}
 
-void bfs(int i, int j){
-	queue<pair<int,int>> q; q.push({i,j});
-	while(!q.empty()){
-		int x=q.front().ff, y=q.front().ss; q.pop();
-		vis[x][y]=1;
-		//show();
-		for(int k=0;k<4;k++){
-			int ni = x+vi[k], nj = y+vj[k];
-			if(ni>=0&&ni<r&&nj>=0&&nj<c&&vis[ni][nj]==0&&(mt[ni][nj]=='.'||mt[ni][nj]=='#')){
-				dist[ni][nj]=dist[x][y]+1;
-				q.push({ni,nj});
+void bfs(){
+	for(auto x:coor){
+		cout<<x.ff<<' '<<x.ss<<endl;
+		vis[x.ff][x.ss]=1;
+		dist[x.ff][x.ss]=0;
+		mt[x.ff][x.ss]='.';
+		int bomb=mt[x.ff][x.ss]-48;
+		queue<pair<int, int>> q; q.push({x.ff, x.ss});
+		while(!q.empty()){
+			int i=q.front().ff, j=q.front().ss; q.pop();
+			for(int k=0; k<4; k++){
+				int ni=i+vi[k], nj=j+vj[k];
+				if(ni>=0&&ni<=bomb&&nj>=0&&nj<bomb&&vis[ni][nj]==0&&(mt[ni][nj]=='.'||mt[ni][nj]=='#')){
+					vis[ni][nj]=1;
+					dist[ni][nj]=dist[i][j]+1;
+					if(dist[ni][nj]!=0&&dist[ni][nj]<=bomb) mt[ni][nj]='.';
+					q.push({ni, nj});
+				}
 			}
-		}
-	}
-}
-
-void bombi(int bomb){
-	for(int i=0; i<r; i++){
-		for(int j=0; j<c; j++){
-			if((dist[i][j]==bomb || dist[i][j]<bomb)&&(mt[i][j]=='.' || mt[i][j]=='#')) mt[i][j]='.';
 		}
 	}
 }
 
 int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 	cin>>r>>c;
 	for(int i=0; i<r; i++){
 		for(int j=0; j<c; j++){
 			cin>>mt[i][j];
-		}
-	}
-	for(int i=0; i<r; i++){
-		for(int j=0; j<c; j++){
-			if(mt[i][j]!='.' || mt[i][j]!='#'){
-				mt[i][j]='.';
-				int bomb=mt[i][j]-48;
-				bfs(i, j);
-				bombi(bomb);
+			if(mt[i][j]!='.' && mt[i][j]!='#'){
+				coor.push_back({i, j});
 			}
-			memset(dist,0,sizeof(dist));;
 		}
 	}
+	bfs();
 	show();
 }
